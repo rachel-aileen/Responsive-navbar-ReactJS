99d09e9d018ac43fc85e1bdce51a9d3c25374aa0
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-04-27 11:04:19 -0700

    a hrefs aligned to the right

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,35 +3,31 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import "../Styles/main.css";
 
 function Navbar() {
-	const navRef = useRef();
+    const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
 
-	return (
-		<header>
-			<h3>LOGO</h3>
-			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">My work</a>
-				<a href="/#">Blog</a>
-				<a href="/#">About me</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+    return (
+        <header>
+            <h3>LOGO</h3>
+            <nav ref={navRef}>
+                <div className="nav-links">
+                    <a href="/#">Home</a>
+                    <a href="/#">My work</a>
+                    <a href="/#">Blog</a>
+                    <a href="/#">About me</a>
+                </div>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
+    );
 }
 
 export default Navbar;
