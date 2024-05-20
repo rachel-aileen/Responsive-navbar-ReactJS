7967ce5b20fa7b7a39ba62b04fd852ffbf011da2
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-05-20 14:19:01 -0700

    overview section layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Hero from './Components/Hero';
+import Overview from './Layouts/Overview';
 
 function App() {
 	return (
 	  <React.Fragment>
 		<Hero />
-		{/* Other components */}
+		<Overview/>
 	  </React.Fragment>
 	);
   }
