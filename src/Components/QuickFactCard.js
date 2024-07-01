1c7808d62c89e03fd5a00dfee3dda795178ff022
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-07-01 14:56:03 -0700

    quick facts card update

--- src/Components/QuickFactCard.js
+++ src/Components/QuickFactCard.js
@@ -12,7 +12,9 @@ export default function QuickFactCard() {
          <GoHome className='quickFactIcon ' />
         <h2 className='quickFactHeaderText'>Building</h2>
         <div className='cardLine'></div>
-        <p className='quickFactsBodyText'>Lorem ipsum</p>
+        <p className='quickFactsBodyText'>Lorem ipsum dolor sit amet,
+        cibsectetyr adip scing elit, sed eiusmod tempor
+           </p>
         </div>
         
   )
